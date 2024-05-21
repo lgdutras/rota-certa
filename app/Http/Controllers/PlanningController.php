22d5c0c74706f4a1ccb2ae361c05276e1a95f7f2
Luiz Gustavo Dutra de Souza <gdutrasouza@gmail.com>
2024-05-21 10:15:20 -0300

    Start planing, setting locale and interests to return places wich user can select to your planning
    Now interests is a required list on payload, wich is used to retrieve data from Google API Places v2 on SearchText Endpoint
    
    On tests, Status Code must be 200 and response json must have displayName field

--- app/Http/Controllers/PlanningController.php
+++ app/Http/Controllers/PlanningController.php
@@ -5,18 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\PlanningRequest;
 use App\Models\TravelPlan;
+use Illuminate\Support\Facades\Http;
 
 class PlanningController extends Controller
 {
     public function setLocale(PlanningRequest $request, TravelPlan $travelPlan) {
 
-    /*
-        TO-DO Save planning data on session using cookies
-        I need to set the project to use cookies instead database and to initiate session on API route
+        $options = [];
 
-        $request->session()->put('city', $request->city);
-        $request->session()->put('state', $request->state);
-    */
-        return response()->json(['message' => 'Travel Planning initiated to ' . $request->city . ' at ' . $request->state, 200]);
+        foreach ($request->interests as $interest) {
+
+            $headers = [
+                'Content-Type' => 'application/json',
+                'X-Goog-Api-Key' => 'AIzaSyDB-JTCWp9RKG78YTYNJhIOGrdVddsW3P0',
+                'X-Goog-FieldMask' => 'places.id,places.displayName,places.formattedAddress,places.priceLevel,places.currentOpeningHours,places.currentSecondaryOpeningHours,places.regularOpeningHours,places.regularSecondaryOpeningHours'
+            ];
+            
+            $url = 'https://places.googleapis.com/v1/places:searchText';
+            $query = 'interests in city/state';
+            $response = Http::withHeaders($headers)->post($url, ['textQuery' => strtr($query, ['interest' => $interest,
+                                                                                                'city' => $request->city,
+                                                                                                'state' => $request->state])]);
+            $option = [$response->body()][0];
+            $option = json_decode($option, true)['places'][0];
+
+            $options = array_merge($options, $option);
+        };
+
+        return $options;
     }
 }
